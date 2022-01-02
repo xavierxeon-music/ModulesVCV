095df87074c99619efd0758a765f3e57e4b41921
Ralf Waspe <rwaspe@icloud.com>
2022-01-02 21:28:01 +0100

    stuff

--- Schweinesystem/src/RandomWalk.cpp
+++ Schweinesystem/src/RandomWalk.cpp
@@ -14,6 +14,11 @@ RandomWalk::RandomWalk()
 
 void RandomWalk::process(const ProcessArgs& args)
 {
+   const bool cutoff = (params[CUTOFF_PARAM].getValue() > 0.1f);
+   lights[BLINK_LIGHT].setBrightness(cutoff ? 1.f : 0.f);
+
+   float speed = params[SPEED_PARAM].getValue();
+
    // The default pitch is C4 = 261.6256f
    float freq = 261.6256f;
 
@@ -27,12 +32,6 @@ void RandomWalk::process(const ProcessArgs& args)
    // Audio signals are typically +/-5V
    // https://vcvrack.com/manual/VoltageStandards
    outputs[RANDOM_OUTPUT].setVoltage(5.f * sine);
-
-   // Blink light at 1Hz
-   blinkPhase += args.sampleTime;
-   if (blinkPhase >= 1.f)
-      blinkPhase -= 1.f;
-   lights[BLINK_LIGHT].setBrightness(blinkPhase < 0.5f ? 1.f : 0.f);
 }
 
 RandomWalkWidget::RandomWalkWidget(RandomWalk* module)
@@ -50,6 +49,6 @@ RandomWalkWidget::RandomWalkWidget(RandomWalk* module)
    // function
    addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.24, 34.063)), module, RandomWalk::SPEED_PARAM));
    addParam(createParamCentered<VCVLatch>(mm2px(Vec(15.24, 64.063)), module, RandomWalk::CUTOFF_PARAM));
-   addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(15.24, 75.81)), module, RandomWalk::BLINK_LIGHT));
-   addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 103.2)), module, RandomWalk::RANDOM_OUTPUT));
+   addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(15.24, 70.81)), module, RandomWalk::BLINK_LIGHT));
+   addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 103.7)), module, RandomWalk::RANDOM_OUTPUT));
 }
