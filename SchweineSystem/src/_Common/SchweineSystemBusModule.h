bd9a69712c39b4d5bee1a7c4b5a0d8475fb6b2ee
Ralf Waspe <rwaspe@icloud.com>
2022-08-15 17:48:05 +0200

    prepare for bus

---
+++ SchweineSystem/src/_Common/SchweineSystemBusModule.h
@@ -0,0 +1,17 @@
+#ifndef SchweineSystemBusModuleH
+#define SchweineSystemBusModuleH
+
+namespace SchweineSystem
+{
+   class BusModule
+   {
+   public:
+      inline BusModule();
+   };
+} // namespace SchweineSystem
+
+#ifndef SchweineSystemBusModuleHPP
+#include "SchweineSystemBusModule.hpp"
+#endif // NOT SchweineSystemBusModuleHPP
+
+#endif // NOT SchweineSystemBusModuleH
---
+++ SchweineSystem/src/_Common/SchweineSystemBusModule.hpp
@@ -0,0 +1,10 @@
+#ifndef SchweineSystemBusModuleHPP
+#define SchweineSystemBusModuleHPP
+
+#include "SchweineSystemBusModule.h"
+
+SchweineSystem::BusModule::BusModule()
+{
+}
+
+#endif // NOT SchweineSystemBusModuleHPP
