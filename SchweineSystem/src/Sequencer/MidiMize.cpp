1e72a966cff40f25d1804b20062f6fcf67449a3c
Ralf Waspe <rwaspe@icloud.com>
2022-09-26 23:09:46 +0200

    prepere midi send

--- SchweineSystem/src/Sequencer/MidiMize.cpp
+++ SchweineSystem/src/Sequencer/MidiMize.cpp
@@ -4,6 +4,68 @@
 
 #include "MidiReplay.h"
 
+MidiMize::VoiceState::VoiceState()
+   : midiValue(0)
+   , gate(false)
+   , velocity(0)
+{
+}
+
+MidiMize::VoiceState::VoiceState(const Note& note, bool gate, uint8_t velocity)
+   : midiValue(note.midiValue)
+   , gate(gate)
+   , velocity(velocity)
+{
+}
+
+MidiMize::VoiceState::VoiceState(const VoiceState& other)
+   : VoiceState()
+{
+}
+
+MidiMize::VoiceState& MidiMize::VoiceState::operator=(const VoiceState& other)
+{
+   midiValue = other.midiValue;
+   gate = other.gate;
+   velocity = other.velocity;
+
+   return *this;
+}
+
+bool MidiMize::VoiceState::operator==(const VoiceState& other) const
+{
+   if (midiValue != other.midiValue)
+      return false;
+   if (gate != other.gate)
+      return false;
+   if (velocity != other.velocity)
+      return false;
+
+   return true;
+}
+
+bool MidiMize::VoiceState::operator!=(const VoiceState& other) const
+{
+   if (midiValue != other.midiValue)
+      return true;
+   if (gate != other.gate)
+      return true;
+   if (velocity != other.velocity)
+      return true;
+
+   return false;
+}
+
+bool MidiMize::DrumState::operator!=(const DrumState& other) const
+{
+   if (active != other.active)
+      return true;
+
+   return false;
+}
+
+// midi mize
+
 MidiMize::MidiMize()
    : Svin::Module()
    , selectKnobList(this)
@@ -11,9 +73,10 @@ MidiMize::MidiMize()
    , pitchInputList(this)
    , gateInputList(this)
    , velocityInputList(this)
-   , gateActive{true, true, true, true}
+   , voiceState{}
+   , velocityMapper(0.0, 10.0, 0.0, 127.0)
    , drumTriggerList(this)
-   , triggerActive{true, true, true, true, true, true, true, true}
+   , drumState{}
 {
    setup();
    registerAsBusModule<MidiBus>();
@@ -64,12 +127,30 @@ MidiMize::MidiMize()
 void MidiMize::process(const ProcessArgs& args)
 {
    MidiBus busMessage;
+   busMessage.noOfChannels = 4;
+   busMessage.runState = Tempo::Running;
+   busMessage.endTick = 1;
 
    for (uint8_t voice = 0; voice < 4; voice++)
    {
       const Midi::Channel channel = selectKnobList[voice]->getValue();
       const std::string channelText = Text::pad(std::to_string(channel), 2);
       channelDisplayList[voice]->setText(channelText);
+
+      const Note note = Note::fromVoltage(pitchInputList[voice]->getVoltage());
+      const bool gate = gateInputList[voice]->isOn();
+      const uint8_t velocity = velocityMapper(velocityInputList[voice]->getVoltage());
+
+      const VoiceState state(note, gate, velocity);
+      if (state == voiceState[voice])
+         continue;
+
+      busMessage.hasEvents = true;
+      if (voiceState[voice].gate) // turn off
+      {
+      }
+
+      voiceState[voice] = state;
    }
 
    sendBusData<MidiBus>(Side::Right, busMessage);
--- SchweineSystem/src/Sequencer/MidiMize.h
+++ SchweineSystem/src/Sequencer/MidiMize.h
@@ -7,6 +7,8 @@ using namespace rack;
 #include <SvinModule.h>
 #include <SvinModuleWidget.h>
 
+#include <Tools/Range.h>
+
 #include <SvinDisplayLCD.h>
 #include <SvinInput.h>
 #include <SvinKnob.h>
@@ -23,6 +25,29 @@ public:
    void process(const ProcessArgs& args) override;
 
 private:
+   struct VoiceState
+   {
+      uint8_t midiValue = 0;
+      bool gate = false;
+      uint8_t velocity = 0;
+
+      VoiceState();
+      VoiceState(const Note& note, bool gate, uint8_t velocity);
+      VoiceState(const VoiceState& other);
+      VoiceState& operator=(const VoiceState& other);
+      bool operator==(const VoiceState& other) const;
+      bool operator!=(const VoiceState& other) const;
+   };
+
+   struct DrumState
+   {
+      bool active = false;
+      dsp::PulseGenerator pulse;
+
+      bool operator!=(const DrumState& other) const;
+   };
+
+private:
    inline void setup();
 
    void load(const Svin::Json::Object& rootObject) override;
@@ -34,10 +59,12 @@ private:
    Svin::Input::List pitchInputList;
    Svin::Input::List gateInputList;
    Svin::Input::List velocityInputList;
-   bool gateActive[4];
+   VoiceState voiceState[4];
+
+   Range::Mapper velocityMapper;
 
    Svin::Input::List drumTriggerList;
-   bool triggerActive[8];
+   DrumState drumState[8];
 };
 
 // widget
