9ee5424ce61125a7e04f329748bade5e25672e21
Ralf Waspe <rwaspe@icloud.com>
2022-10-08 16:46:54 +0200

    conductor is header only

--- SchweineSystem/src/Sequencer/Conductor.h
+++ SchweineSystem/src/Sequencer/Conductor.h
@@ -7,10 +7,19 @@
 class Conductor : public Contour::Poly, public Grooves
 {
 public:
-   Conductor();
+   Conductor()
+      : Abstract::SegmentCrawler()
+      , Contour::Poly()
+      , Grooves()
+   {
+   }
 
 public:
-   void update(const uint8_t& newDefaultDivision, const uint32_t newSegmentCount) override;
+   void update(const uint8_t& newDefaultDivision, const uint32_t newSegmentCount) override
+   {
+      Contour::Poly::update(newDefaultDivision, newSegmentCount);
+      Grooves::update(newDefaultDivision, newSegmentCount);
+   }
 };
 
 #endif // NOT ConductorH
