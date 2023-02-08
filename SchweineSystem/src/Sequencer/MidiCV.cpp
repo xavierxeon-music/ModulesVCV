c7a689d0db1dc0a2795e48939988eae44ffa1c7c
Ralf Waspe <rwaspe@icloud.com>
2023-02-08 08:53:45 +0100

    midi cv working

--- SchweineSystem/src/Sequencer/MidiCV.cpp
+++ SchweineSystem/src/Sequencer/MidiCV.cpp
@@ -31,22 +31,26 @@ void MidiCV::process(const ProcessArgs& args)
 
    const bool isRunning = (Tempo::Running == busMessage.runState) || (Tempo::FirstTick == busMessage.runState);
    if (!isRunning)
-      resetBuffer();
-
-   for (uint8_t channel = 0; channel < busMessage.noOfChannels; channel++)
    {
-      if (!busMessage.channels[channel].hasEvents)
-         continue;
+      resetBuffer();
+   }
+   else
+   {
+      for (uint8_t channel = 0; channel < busMessage.noOfChannels; channel++)
+      {
+         if (!busMessage.channels[channel].hasEvents)
+            continue;
 
-      const Midi::Sequence::MessageList& messageList = busMessage.channels[channel].messageList;
-      for (const Bytes& message : messageList)
-         prcocessMessage(message);
+         const Midi::Sequence::MessageList& messageList = busMessage.channels[channel].messageList;
+         for (const Bytes& message : messageList)
+            prcocessMessage(message);
+      }
    }
 
    for (uint8_t index = 0; index < 16; index++)
    {
-      pitchOutput.setVoltage(pitchBuffer[0], index);
-      gateOutput.setOff(gateBuffer[index]);
+      pitchOutput.setVoltage(pitchBuffer[index], index);
+      gateOutput.setActive(gateBuffer[index], index);
       velocityOutput.setVoltage(velocityBuffer[index], index);
    }
 }
@@ -54,18 +58,21 @@ void MidiCV::process(const ProcessArgs& args)
 void MidiCV::noteOn(const Midi::Channel& channel, const uint8_t& midiNote, const Midi::Velocity& velocity)
 {
    Note note = Note::fromMidi(midiNote);
-   pitchBuffer[channel] = note.voltage;
-   gateBuffer[channel] = true;
-   velocityBuffer[channel] = velocity;
+
+   const uint8_t index = channel - 1;
+   pitchBuffer[index] = note.voltage;
+   gateBuffer[index] = true;
+   velocityBuffer[index] = velocity;
 }
 
 void MidiCV::noteOff(const Midi::Channel& channel, const uint8_t& midiNote)
 {
    (void)midiNote;
 
-   pitchBuffer[channel] = 0.0;
-   gateBuffer[channel] = false;
-   velocityBuffer[channel] = 0.0;
+   const uint8_t index = channel - 1;
+   pitchBuffer[index] = 0.0;
+   gateBuffer[index] = false;
+   velocityBuffer[index] = 0.0;
 }
 
 void MidiCV::resetBuffer()
